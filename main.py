83ef0e64f265d701d166c3dd2e8330d4f7ab5f66
Nate Michalov <nmichalov@gmail.com>
2022-07-26 09:32:53 -0400

    added test dir to make vlun severity ambiguous

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def xss_2():
 
 @app.route("/xss_3", methods = ['GET'])
 def xss_3():
-    username = 'user'
-    return "<p>Welcome to our app %s!" % username 
+    user_id = request.cookies.get('UserID')
+    return "<p>Welcome to our app %s!" % user_id 
 
 @app.route("/code_injection_1", methods = ['GET', 'POST'])
 def code_injection_1():
@@ -35,4 +35,25 @@ def code_injection_1():
         _download_git(filename)
 
 def _download_git(filename):
+        os.system("git clone --quiet %s" % (filename))
+
+@app.route("/code_injection_2", methods = ['GET', 'POST'])
+def code_injection_2():
+    approved_files = {'c' : "config", 's' : "system"}
+    filename = request.args.get("filename")
+    target_file = approved_files.get(filename)
+    _download_git_2(target_file)
+
+def _download_git_2(filename):
+        os.system("git clone --quiet %s" % (filename))
+
+@app.route("/code_injection_3", methods = ['GET', 'POST'])
+def code_injection_3():
+    filename = request.args.get("filename")
+    approved_files = []
+    approved_files.append(filename)
+    target_file = approved_files[0]
+    _download_git_3(target_file)
+
+def _download_git_3(filename):
         os.system("git clone --quiet %s" % (filename))
---
+++ test/vulns.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import os
+import sys
+import traceback
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route("/code_injection_1", methods = ['GET', 'POST'])
+def code_injection_1():
+    filename = request.args.get("filename")
+    _download_git(filename)
+
+def _download_git(filename):
+        os.system("git clone --quiet %s" % (filename))
+
+@app.route("/xss", methods = ['GET'])
+def xss():
+    username = request.args.get("username")
+    return "<p>Welcome to our app %s!" % username
+
+@app.route("/info_leak", methods = ['GET'])
+def info_leak():
+    try:
+        val = x[1]
+    except Exception:
+         return traceback.print_last()
